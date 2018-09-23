92c3a395dfae78d0576d8000e5f8b03e0ac7d7a2
Lei Wang <lewang@thoughtworks.com>
2018-09-23 22:40:28 +0800

    connect login api for redux and resrfulAPI

--- src/components/login/components/Login.js
+++ src/components/login/components/Login.js
@@ -2,40 +2,73 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { Modal, Button} from "react-bootstrap";
 import '../style.scss';
+import {authenticationReducer} from "../../../dataStore/authenticationReducer";
 
-export default ({onLogin, logged}) => (
-  <div>
-    {
-      !logged &&
-      <div className="static-modal">
-        <Modal.Dialog>
-        <Modal.Header>
-        <Modal.Title>Login</Modal.Title>
-        </Modal.Header>
+const authenticationServer = '/login';
 
-        <Modal.Body>
-            <div>
-                <label className="login-label" htmlFor="username">Username:</label>
-                <input ref={(ref) => this.username = ref} id="username" />
-            </div>
-            <div>
-                <label className="login-label" htmlFor="password">Password:</label>
-                <input ref={(ref) => this.password = ref} type="password" id="password" />
-            </div>
-        </Modal.Body>
+export default class Login extends React.Component{
 
-        <Modal.Footer>
-        <Button bsStyle="primary" onClick={() => {
-            if (this.username.value === 'tw' && this.password.value === 'tw') {
-                onLogin();
+    constructor(props){
+        super(props);
+    }
+
+    createUserSession = (username,password) => {
+        return fetch(authenticationServer,{
+            method:'POST',
+            headers:{
+                'Content-Type' : 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({"name":username,"password":password})
+        }).then(response => {
+            if (response.ok){
+                return response.json();
             }
-        }}>Login</Button>
-        </Modal.Footer>
-        </Modal.Dialog>
-        </div>
+        }).then( data  => {
+          if (data !== undefined) {
+              this.props.onLogin();
+          }
+        }).catch(error => {
+            console.error('Error:', error)
+        });
+    };
+
+    render() {
+      const { logged } = this.props;
+        return (
+            <div>
+                {
+                    !logged &&
+                    <div className="static-modal">
+                        <Modal.Dialog>
+                            <Modal.Header>
+                                <Modal.Title>Login</Modal.Title>
+                            </Modal.Header>
+
+                            <Modal.Body>
+                                <div>
+                                    <label className="login-label" htmlFor="username">Username:</label>
+                                    <input ref={(ref) => this.username = ref} id="username" />
+                                </div>
+                                <div>
+                                    <label className="login-label" htmlFor="password">Password:</label>
+                                    <input ref={(ref) => this.password = ref} type="password" id="password" />
+                                </div>
+                            </Modal.Body>
+
+                            <Modal.Footer>
+                                <Button bsStyle="primary" onClick={() => {
+                                  this.createUserSession(this.username.value,this.password.value);
+                                }}>Login</Button>
+                            </Modal.Footer>
+                        </Modal.Dialog>
+                    </div>
+                }
+                {
+                    logged && <Redirect to="/home" />
+                }
+            </div>
+        );
     }
-    {
-      logged && <Redirect to="/home" />
-    }
-  </div>
-);
+
+}
--- src/index.js
+++ src/index.js
@@ -4,13 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-fetch('/todos')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (myJson) {
-        console.log(myJson);
-    });
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
