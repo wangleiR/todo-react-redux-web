3da9b7e10028b6d5c1287fa0f048f9bc8a3cd128
Lei Wang <lewang@thoughtworks.com>
2018-09-24 21:20:08 +0800

    complete chart function

--- src/components/chart/component/Chart.js
+++ src/components/chart/component/Chart.js
@@ -1,11 +1,12 @@
 import React from "react";
 import {Pie} from 'react-chartjs-2';
+import moment from "moment";
 
 export default class Chart extends  React.Component{
 
     constructor(props){
         super(props);
-        this.state = {
+        this.chartStatusData = {
             labels: [
                 'To Do',
                 'In Progress',
@@ -23,39 +24,84 @@ export default class Chart extends  React.Component{
                 '#FFCE56'
             ],
         };
-        this.calculDifferentItemStatusNumber(this.props.lists);
+
+        this.chartDateData = {
+            labels: [
+                'Out of date',
+                'In 1 day',
+                'In 3 day'
+            ],
+            data: [0, 0, 0],
+            backgroundColor: [
+                '#FF6384',
+                '#36A2EB',
+                '#FFCE56'
+            ],
+            hoverBackgroundColor: [
+                '#EF6384',
+                '#36C2EB',
+                '#FECE56'
+            ],
+        };
+
     }
 
-    getData =() => {
+    getData =(chartData) => {
         return {
-                labels : this.state.labels,
+                labels : chartData.labels,
                 datasets : [{
-                    data: this.state.data,
-                    backgroundColor: this.state.backgroundColor,
-                    hoverBackgroundColor: this.state.hoverBackgroundColor,
+                    data: chartData.data,
+                    backgroundColor: chartData.backgroundColor,
+                    hoverBackgroundColor: chartData.hoverBackgroundColor,
                 }]
             };
     };
 
-    calculDifferentItemStatusNumber = lists => {
+    calculDifferentItemStatusNumber = (lists) => {
+        this.chartStatusData.data = [0,0,0];
         lists.map(item => {
             if (item.status === "To Do") {
-                return this.state.data[0]++;
+                return this.chartStatusData.data[0]++;
             }else if (item.status === "In Progress") {
-                return this.state.data[1]++;
+                return this.chartStatusData.data[1]++;
             }else if (item.status === "Blocked") {
-                return  this.state.data[2]++;
+                return  this.chartStatusData.data[2]++;
             }
         });
-        this.setState({
-            data: this.state.data,
+
+        this.chartDateData.data = [0,0,0];
+
+        let now = moment().format('YYYY-MM-DD');
+        lists.map(item => {
+            let  dueDate = moment(item.dueDate).format('YYYY-MM-DD');
+            if (moment(dueDate).isBefore(now)) { //Out of date
+                return this.chartDateData.data[0]++;
+            }else if (Math.abs(moment(now).diff(moment(dueDate))) <= 86400000) { //In 1 day
+                return this.chartDateData.data[1]++;
+            }else if (Math.abs(moment(now).diff(moment(dueDate))) <= 259200000) { //In 3 day
+                return  this.chartDateData.data[2]++;
+            }
         });
+
     };
 
     render() {
         return (
             <div>
-                <Pie data={this.getData()}/>
+                {
+                    this.calculDifferentItemStatusNumber(this.props.lists)
+                }
+                <Pie
+                    data={this.getData(this.chartStatusData)}
+                    width={300}
+                    height={100}
+                />
+                <Pie
+                    data={this.getData(this.chartDateData)}
+                    width={300}
+                    height={100}
+                />
+
             </div>
         );
     }
